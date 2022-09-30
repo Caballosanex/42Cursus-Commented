b058202d227604a680da3bfa958e0f73b0b027a1
Àlex Sánchez Blabia <alexsanchezblabia@gmail.com>
2022-09-30 13:05:15 +0200

    Add files via upload

---
+++ CC00/libFT/ft_calloc.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alexsanc <alexsanc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/30 11:10:18 by alexsanc          #+#    #+#             */
+/*   Updated: 2022/09/30 11:37:53 by alexsanc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	size_t	i;
+	void	*ptr;
+
+	i = 0;
+	ptr = malloc(count * size);
+	if (ptr == NULL)
+		return (NULL);
+	while (i < count * size)
+	{
+		((char *)ptr)[i] = 0;
+		i++;
+	}
+	return (ptr);
+}
