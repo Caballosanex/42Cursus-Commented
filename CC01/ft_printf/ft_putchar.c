2ce4d219f6dd4616769e9f12b660f0690c30b493
Àlex Sánchez Blabia <alexsanchezblabia@gmail.com>
2022-11-08 10:55:28 +0100

    Known Good Conversions Upload
    Changed ft_putchar, ft_putnbr and ft_putstr. They all pass Paco. Not NFriendly. Commit for tracking

--- CC01/ft_printf/ft_putchar.c
+++ CC01/ft_printf/ft_putchar.c
@@ -6,13 +6,13 @@
 /*   By: alexsanc <alexsanc@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 10:59:55 by alexsanc          #+#    #+#             */
-/*   Updated: 2022/10/25 14:04:51 by alexsanc         ###   ########.fr       */
+/*   Updated: 2022/11/07 14:39:15 by alexsanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_putchar(int c)
+int ft_putchar(char c)
 {
 	if (write(1, &c, 1) != 1)
 		return (ERR_NUM);
--- CC01/ft_printf/ft_putstr.c
+++ CC01/ft_printf/ft_putstr.c
@@ -6,24 +6,29 @@
 /*   By: alexsanc <alexsanc@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 12:26:46 by alexsanc          #+#    #+#             */
-/*   Updated: 2022/11/03 11:49:05 by alexsanc         ###   ########.fr       */
+/*   Updated: 2022/11/07 14:30:15 by alexsanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_putstr(char *str)
+// ft_putstr that handles NULL and returns ERR_NUM in case of error
+int ft_putstr(char *s)
 {
-	int	i;
+	int i;
 
 	i = 0;
-	if (!str)
-		return (ERR_NUM);
-	while (str[i])
+	if (s == NULL)
 	{
-		if (ft_putchar(str[i]) == ERR_NUM)
+		if (write(1, "(null)", 6) != 6)
 			return (ERR_NUM);
-		i++;
+		return (6);
+	}
+		while (s[i])
+		{
+			if (ft_putchar(s[i]) == ERR_NUM)
+				return (ERR_NUM);
+			i++;
 	}
 	return (i);
 }
